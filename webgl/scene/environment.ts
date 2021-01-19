6eafb452117c217e4d86698c63e31d5d925d7a45
Christian Volkmann <christian.volkmann@rwth-aachen.de>
2021-01-19 02:03:49 +0100

    Refactpr spacey donut

--- webgl/scene/environment.ts
+++ webgl/scene/environment.ts
@@ -1,65 +1,92 @@
 import * as BABYLON from 'babylonjs'
-import { imageProcessingFunctions } from 'babylonjs/Shaders/ShadersInclude/imageProcessingFunctions'
 
-export function init(scene: BABYLON.Scene) {
-  const height = 10
-  const width = 10
-  const segments = 10
-
-  const ground = BABYLON.Mesh.CreateGround(
-    'ground',
+function makeSpaceyDonutMesh(
+  scene: BABYLON.Scene,
+  width: number = 10,
+  height: number = 10,
+  segments: number = 10
+) {
+  const parent = new BABYLON.TransformNode('spaceyDonut', scene)
+  const plane = BABYLON.Mesh.CreateGround(
+    'plane',
     width,
     height,
     segments,
     scene,
     true
   )
-  const gmat = new BABYLON.StandardMaterial('gmat', scene)
-  gmat.diffuseColor = new BABYLON.Color3(0.4, 0.4, 0.8)
-  ground.material = gmat
-  ground.convertToFlatShadedMesh()
+  const planeMat = new BABYLON.StandardMaterial('planeMat', scene)
+  planeMat.diffuseColor = new BABYLON.Color3(0.4, 0.4, 0.8)
+  plane.material = planeMat
+  plane.convertToFlatShadedMesh()
+  plane.parent = parent
 
-  const ground2 = BABYLON.Mesh.CreateGround(
-    'ground2',
+  const frame = BABYLON.Mesh.CreateGround(
+    'frame',
     width,
     height,
     segments,
     scene,
     true
   )
-  const gmat2 = new BABYLON.StandardMaterial('gmat2', scene)
-  gmat2.diffuseColor = new BABYLON.Color3(1, 1, 1)
-  gmat2.wireframe = true
-  ground2.material = gmat2
-  ground2.convertToFlatShadedMesh()
+  const frameMat = new BABYLON.StandardMaterial('frameMat', scene)
+  frameMat.diffuseColor = new BABYLON.Color3(1, 1, 1)
+  frameMat.wireframe = true
+  frame.material = frameMat
+  frame.convertToFlatShadedMesh()
+  frame.parent = parent
 
-  const positions = ground.getVerticesData(BABYLON.VertexBuffer.PositionKind)
-  const totalFrames = 60
+  return { parent, plane, frame }
+}
 
-  const scaleTo = (x: number, min: number, max: number) => (max - min) * x + min
-  const waveMod = (p: number) => -Math.cos(p * 2 * Math.PI)
+// Returns a function that maps [0,1) into the target domain
+const scaleTo = (min: number, max: number) => (x: number) =>
+  (max - min) * x + min
 
-  const min = 0
-  const max = 10
-  const mod = (p: number, i: number) =>
-    scaleTo(waveMod(p + i * 2 * Math.PI), min, max)
+// Maps from [0,1) to [0, 1)
+// Input resembles progress %
+// Output resembles point between min and max along the output axis
+const waveMod = (p: number) => -Math.cos(p * 2 * Math.PI)
+
+function animateSpaceyDonut(
+  scene: BABYLON.Scene,
+  spaceyDonut: ReturnType<typeof makeSpaceyDonutMesh>,
+  min: number = 0,
+  max: number = 10,
+  totalFrames: number = 60
+) {
+  const posBuf = spaceyDonut.plane.getVerticesData(
+    BABYLON.VertexBuffer.PositionKind
+  )
+
+  const scale = scaleTo(min, max)
+  // p corresponds to progress of one full cycle
+  // i corresponds to a phasis shift progress
+  const mod = (p: number, i: number) => scale(waveMod(p + i * 2 * Math.PI))
 
   let counter = 0
-  ground.registerBeforeRender(() => {
-    const p = counter / totalFrames
-    const posNew = positions.map((x: number, i: number) => mod(p, i))
-    ground.updateVerticesData(
+  spaceyDonut.plane.registerBeforeRender(() => {
+    const progress = counter / totalFrames
+    const posNew = posBuf.map(
+      (_: number, phaseIndex: number) => mod(progress, phaseIndex) // ! phaseProgress to unit int
+    )
+    spaceyDonut.plane.updateVerticesData(
       BABYLON.VertexBuffer.PositionKind,
       posNew,
       false,
       true
     )
-    ground2.updateVerticesData(
+    spaceyDonut.frame.updateVerticesData(
       BABYLON.VertexBuffer.PositionKind,
       posNew,
       false,
       true
     )
-    counter++
+    counter = (counter + 1) % totalFrames
   })
+}
+
+export function init(scene: BABYLON.Scene) {
+  const spaceyDonut = makeSpaceyDonutMesh(scene)
+  animateSpaceyDonut(scene, spaceyDonut)
 }
